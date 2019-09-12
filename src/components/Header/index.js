43e24a62ff5efe07e5962139719850bf2cbd7c87
PauloRev <paulohenriquerev@gmail.com>
2019-09-12 20:17:28 -0300

    Added styled-components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <Navbar color="light" light expand="md">
+        <div className="container">
+          <NavbarBrand>
+            Minhas Séries
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={open} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  Séries
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  Gêneros
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
+      </Navbar>
+    </header>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <h1>Home</h1>
     </div>
   );
 }
